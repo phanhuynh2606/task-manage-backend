dac995e913530607feb2a43d8d95d073c1723629
phanhuynh2606 <huynhpvhe170150@fpt.edu.vn>
2023-11-15 10:01:13 +0700

    Sửa cấu hình

--- api/v1/routes/task.route.js
+++ api/v1/routes/task.route.js
@@ -1,25 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
+const controller = require("../controllers/task.controller");
 
-const Task = require("../../../models/task.model");
+router.get("/", controller.index);
 
-router.get("/", async (req, res) => {
-  const tasks = await Task.find({
-    deleted: false,
-  });
-  // console.log(tasks);
-  res.json(tasks);
-});
-
-router.get("/detail/:id", async (req, res) => {
-  const id = req.params.id;
-  const tasks = await Task.findOne({
-    _id: id,
-    deleted: false,
-  });
-  // console.log(tasks);
-  res.json(tasks);
-});
+router.get("/detail/:id", controller.detail);
 
 module.exports = router;
