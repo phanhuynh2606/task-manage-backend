d313d7d9490d6e65720f82d2f37cf4e07233749b
phanhuynh2606 <huynhpvhe170150@fpt.edu.vn>
2023-11-21 07:52:42 +0700

    Tính năng đăng nhập

--- api/v1/controllers/user.controller.js
+++ api/v1/controllers/user.controller.js
@@ -13,7 +13,7 @@ module.exports.register = async (req, res) => {
       code: 400,
       message: "Email đã tồn tại",
     });
-    return ;
+    return;
   }
   const infoUser = {
     fullName: req.body.fullName,
@@ -21,14 +21,46 @@ module.exports.register = async (req, res) => {
     password: md5(req.body.password),
     token: generate.generateRandomString(30),
   };
-  
+
   const user = new User(infoUser);
   await user.save();
   const token = user.token;
-  res.cookie("token",token);
+  res.cookie("token", token);
   res.json({
     code: 200,
     message: "Đăng kí thành công",
+    token: token,
+  });
+};
+
+// [POST] /api/v1/users/login
+module.exports.login = async (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  const user = await User.findOne({
+    email: email,
+    deleted: false,
+  });
+  if (!user) {
+    res.json({
+      code: 400,
+      message: "Email không tồn tại",
+    });
+    return;
+  }
+  if (md5(password) != user.password) {
+    res.json({
+      code: 400,
+      message: "Sai mật khẩu",
+    });
+    return;
+  }
+  const token = user.token;
+  res.cookie("token", token);
+  res.json({
+    code: 200,
+    message: "Đăng nhập thành công",
     token: token,
   });
 };
