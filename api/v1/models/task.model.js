e0a6862337dc4226922cd4caf91b69dbc2bb1d69
phanhuynh2606 <huynhpvhe170150@fpt.edu.vn>
2023-11-21 14:52:05 +0700

    Lưu ID người tạo

--- api/v1/models/task.model.js
+++ api/v1/models/task.model.js
@@ -7,6 +7,7 @@ const taskSchema = new mongoose.Schema(
     content: String,
     timeStart: Date,
     timeFinish: Date,
+    createBy : String,
     deleted: {
       type: Boolean,
       default: false,
